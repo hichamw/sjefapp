ecf1cf1464d377cf7912cbf5882f77e7859cb738
Hicham Wakrim <0897587@hr.nl>
2021-01-22 19:32:12 +0100

    yes

--- src/ProductOverview/ProductOverview.js
+++ src/ProductOverview/ProductOverview.js
@@ -38,79 +38,79 @@ class ProductOverview extends Component {
 
     render() {   
         // backup als API niet meer werkt
-        // let products = [
-        //   {
-        //     name: "Boerenkool",
-        //     distance: 294,
-        //     itemsAvailable: 5,
-        //     unit: '300 g',
-        //     price: 0.18,
-        //     lat: 51.921118,
-        //     long: 4.472474,
-        //     imgId: 6,
-        //   },
-        //   {
-        //     name: "Blueband Kookroom",
-        //     distance: 223,
-        //     itemsAvailable: 10,
-        //     unit: '250 ml',
-        //     price: 0.30,
-        //     lat: 51.923190, 
-        //     long: 4.477803,
-        //     imgId: 7,
-        //   },
-        //   {
-        //     name: "Prei",
-        //     distance: 354,
-        //     itemsAvailable: 8,
-        //     unit: '1 st',
-        //     price: 0.12,
-        //     lat: 51.925066, 
-        //     long: 4.469515,
-        //     imgId: 3,
-        //   },
-        //   {
-        //     name: "AH Smoothie",
-        //     distance: 294,
-        //     itemsAvailable: 5,
-        //     unit: '300 g',
-        //     price: 0.50,
-        //     lat: 51.921118,
-        //     long: 4.472474,
-        //     imgId: 4
-        //   },
-        //   {
-        //     name: "Broccoli",
-        //     distance: 381,
-        //     itemsAvailable: 7,
-        //     unit: '350 g',
-        //     price: 0.20,
-        //     lat: 51.913174,
-        //     long: 4.458649,
-        //     imgId: 2,
-        //   },
-        //   {
-        //     name: "Kiwi's",
-        //     distance: 294,
-        //     itemsAvailable: 4,
-        //     unit: '1 st',
-        //     price: 0.10,
-        //     lat: 51.916484, 
-        //     long: 4.485768,
-        //     imgId: 8,
-        //   },
-        //   {
-        //     name: "Trostomaten",
-        //     distance: 693,
-        //     itemsAvailable: 4,
-        //     unit: '5 st',
-        //     price: 0.21,
-        //     lat: 51.916362, 
-        //     long: 4.470641,
-        //     imgId: 9,
-        //   },
+        let products = [
+          {
+            name: "Boerenkool",
+            distance: 294,
+            itemsAvailable: 5,
+            unit: '300 g',
+            price: 0.18,
+            lat: 51.921118,
+            long: 4.472474,
+            imgId: 6,
+          },
+          {
+            name: "Blueband Kookroom",
+            distance: 223,
+            itemsAvailable: 10,
+            unit: '250 ml',
+            price: 0.30,
+            lat: 51.923190, 
+            long: 4.477803,
+            imgId: 7,
+          },
+          {
+            name: "Prei",
+            distance: 354,
+            itemsAvailable: 8,
+            unit: '1 st',
+            price: 0.12,
+            lat: 51.925066, 
+            long: 4.469515,
+            imgId: 3,
+          },
+          {
+            name: "AH Smoothie",
+            distance: 294,
+            itemsAvailable: 5,
+            unit: '300 g',
+            price: 0.50,
+            lat: 51.921118,
+            long: 4.472474,
+            imgId: 4
+          },
+          {
+            name: "Broccoli",
+            distance: 381,
+            itemsAvailable: 7,
+            unit: '350 g',
+            price: 0.20,
+            lat: 51.913174,
+            long: 4.458649,
+            imgId: 2,
+          },
+          {
+            name: "Kiwi's",
+            distance: 294,
+            itemsAvailable: 4,
+            unit: '1 st',
+            price: 0.10,
+            lat: 51.916484, 
+            long: 4.485768,
+            imgId: 8,
+          },
+          {
+            name: "Trostomaten",
+            distance: 693,
+            itemsAvailable: 4,
+            unit: '5 st',
+            price: 0.21,
+            lat: 51.916362, 
+            long: 4.470641,
+            imgId: 9,
+          },
           
-        // ];
+        ];
 
         return (
           <div className="mainContainerProductList">
@@ -152,9 +152,9 @@ class ProductOverview extends Component {
             </div>
 
             {this.props.mapActive ? (
-              <MapView data={this.state.items} />
+              <MapView data={products} />
             ) : (
-              <ListView data={this.state.items} />
+              <ListView data={products} />
             )}
           </div>
         );
